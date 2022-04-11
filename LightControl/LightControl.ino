e5c78e0c811ea5d03b12b6295c0f3f70324c5231
D41Robot <dkeats12@gmail.com>
2022-04-11 18:25:11 -0500

    Operating Code

--- LightControl/LightControl.ino
+++ LightControl/LightControl.ino
@@ -2,19 +2,19 @@
  * The Arduino will read in a string from Serial and then
  * update the approperiate digital out state.
  * 
+ * The user only needs to type a string as long as the number 
+ * of channels being used
  */
 
-int channels[] = {0,1,2,3,4}; //The Digital Pins in use
-//int prevState[4];
-//int currentState[] = {0,0,0,0,0};
+int channels[] = {13,12,11,10,9}; //The Digital Pins in use
+
 
 void setup(){
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-
+/*
+  for(int i = 0; i < sizeof(channels)-1; i++){
+    pinMode(channels[i], OUTPUT);
+  }
+*/
   Serial.begin(9600);
 
   Serial.println("Welcome to the interface");
@@ -26,25 +26,25 @@ void loop() {
     String currentIn = Serial.readString();
     Serial.println(currentIn);
     //Only go through the characters of interest.
-    for (int i=0; i < sizeof(channels) -1 ; i++){
+    for (int i=0; i < 5 ; i++){
       //If the selected channel control is 0 turn off
       if(currentIn[i] == '0'){
+        pinMode(channels[i], INPUT);
         digitalWrite(channels[i], LOW);
-        //prevState[i] = currentIn[i];
         Serial.println("Turned off line: " + String(channels[i]));
       }
+      
       //If the selected channel control is 1 turn on
       else if(currentIn[i] == '1'){
+        pinMode(channels[i], OUTPUT);
         digitalWrite(channels[i], HIGH);
-        //prevState[i] = currentIn[i];
         Serial.println("Turned on line: " + String(channels[i]));
       }
+      
       //something else was found in the message
       else{
         Serial.println("Transmission had an error!");
-      }
-      
+      }  
     }
   }
-
 }
